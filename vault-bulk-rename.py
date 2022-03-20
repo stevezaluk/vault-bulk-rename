b8226a09ce1e59c0575f5c7161cd53dc5b2b56a8
Steven Zaluk <stevezaluk@gmail.com>
2022-03-20 14:46:09 -0400

    Modified usage

--- vault-bulk-rename.py
+++ vault-bulk-rename.py
@@ -1,29 +1,41 @@
-import os, sys
+import os, sys, argparse
+
+from core.cli import Cli
 
 def usage():
     print("vault-bulk-rename - A powerful bulk renaming utility for media files")
     print("")
     print("Basic Operations")
     print("     -h --help : Print this page")
-    print("     -i, --info [path] : Get info on a directory or file")
+    print("     -i, --input : Input path")
+    print("     -I, --info [path] : Get info on a directory or file")
     print("     -c, --check [path] : Check to see if a media file and/or directory is on VAULT")
+    print("     -v, --verbose : Enable verbosity")
     print("Renaming: ")
     print("     -r, --rename : Rename a media file or directory")
-    print("         --extensions [ext] : Only rename certain extensions")
     print("         --preset [preset_name] : Search for a preset to rename against")
+    print("Auto Renaming: ")
+    print("     -ar, --auto-rename [agent] : Auto Rename a media file or directory and match it to a Database")
+    print("         --aired-order : Rename against aired order")
+    print("         --dvd-order : Rename against DVD order")
+    print("         --absolute-order : Rename against absolute order")
+    print(" Walk Options: ")
+    print("         --recursive : Walk files recursivly")
+    print("         --top-down : Walk the directory top down")
+    print("         --dry-run : Do everything except rename the file")
+    print("         --extensions [ext] : Only rename files with certain extensions")
     print("         --folder-regex : Match folders against regex")
     print("         --file-regex : Match files against regex")
-    print("         --recursive : Walk files recursivly")
-    print("         --encoder [encoder] : Only rename files with this encoder")
-    print("         --dry-run : Do everything except rename the file")
-    print("Auto Renaming: ")
-    print("     -ar, --auto-rename : Auto Rename a media file or directory and match it to a Database")
-    print("         --tv-db : Default. Match against shows and movies on TVdB")
-    print("         --imdb : Match against shows and movies on IMDB")
-    print("         --youtube : Match against videos on youtube")
+    print("         --folder-output : Folder renaming pattern")
+    print("         --file-output : File renaming pattern")
+    print("         --use-parsed : Use whatever is matched in file_regex to rename the file")
+    print("         --embed-subtitles [.srt] : Embed a subtitles file into a media file (mp4, mkv)")
+    print("         --find-subtitles : Find subtitles for the piece of media and embed them")
+    print("         --reorder-audio-tracks : Reorder audio tracks to set english ones first")
 
 def main():
-    usage()
+    c = Cli()
+    c.test()
 
 if __name__ == '__main__':
     main()
